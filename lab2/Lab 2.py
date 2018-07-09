6e8d45eb3b3218355983e12073049facaeb34806
Marcel Albus <marcel.albus@ipa.fraunhofer.de>
2018-07-09 15:56:21 +0200

    [CODE] lab 2 improvement

--- lab2/Lab 2.py
+++ lab2/Lab 2.py
@@ -30,11 +30,11 @@ import matplotlib.pyplot as plt
 
 
 # ## Introduction to Chainer
-# 
+#
 # Chainer can be understood as Numpy plus the ability to record the computation graph of numerical operations to enable Automatic Differentiation. (Chainer actually also offers many other things; for example, a Numpy equivalent library that runs on GPU, but we will ignore them for now)
 
 # Let's illustrate how Chainer works by a simple 1D regression task.
-# 
+#
 # Suppose we have observations from the following model $y = w x + b + \epsilon$ where $\epsilon \sim \mathcal{N}(0, 0.1)$ and the task is to estimate the linear model parameters $w, b$ from data.
 
 # In[3]:
@@ -43,7 +43,7 @@ import matplotlib.pyplot as plt
 # first generate some observations
 true_a = 1.3
 true_b = 0.4
-data_x = (np.arange(100) / 99.0 - .5).astype(np.float32) # Chainer assumes all the cpu computation is done in float32
+data_x = (np.arange(100) / 99.0 - .5).astype(np.float32)  # Chainer assumes all the cpu computation is done in float32
 data_y = (data_x * true_a + true_b + np.random.randn(*data_x.shape) * 0.1).astype(np.float32)
 _ = plt.scatter(data_x, data_y, c='b')
 
@@ -52,21 +52,26 @@ _ = plt.scatter(data_x, data_y, c='b')
 
 # In[4]:
 
-
-model = L.Linear(in_size=1, out_size=1) # input is 1D data and output is also 1D data
+# chianer.links.Linear --> L.Linear
+# Linear layer (a.k.a. fully-connected layer)
+# W = Weight parameter (Matrix)
+# b = bias parameter (Vector)
+# y = mx + c
+# m = W ; b = c
+model = L.Linear(in_size=1, out_size=1)  # input is 1D data and output is also 1D data
 
 # Chainer will randomly initialize `w` and `b` for us.
 # we can take a look at their values
 print("w:", model.W)
 print("b:", model.b)
 
-# model.W and model.b have type `chainer.Variable`, 
+# model.W and model.b have type `chainer.Variable`,
 #   which is a wrapper around Numpy array
 assert isinstance(model.W, chainer.Variable)
 
 # operations that involve `chainer.Variable` will produce
 #   `chainer.Variable` and this records the computation graph
-var_result = model.W + 123 # some random computation
+var_result = model.W + 123  # some random computation
 print("Operations on chainer.Variable: %s, type: %s" % (var_result, type(var_result)))
 
 # the underlying numpy array can be accessed by `data` attribute
@@ -78,14 +83,19 @@ print("numpy arrays:", model.W.data, var_result.data)
 
 # A chainer link is a callable object. calling it performs the
 #   forward computation. (in this case, it performs Wx + b)
-model_y = model(data_x[:, None]) # chainer's link usually assumes input is [Batch Size, Input Dimension]
+model_y = model(data_x[:, None])  # chainer's link usually assumes input is [Batch Size, Input Dimension]
 # `model_y` is a chainer variable so we use `.data` to access its numpy array for plotting
 
 # we can plot the model's current fit in red. it should be terrible because we haven't trained it yet
-_ = plt.plot(data_x, model_y.data[:,0], c='r')
+_ = plt.plot(data_x, model_y.data[:, 0], c='r')
 _ = plt.scatter(data_x, data_y, c='b')
 _ = plt.title("Initial model")
-
+###############
+# USER OUTPUT
+###############
+_ = plt.grid()
+plt.clf()
+# _ = plt.show()
 
 # In[11]:
 
@@ -113,41 +123,68 @@ print("dloss/dW", model.W.grad)
 
 
 # Now that we know how to calculate gradients, we can code up a simple loop to perform gradient descent to train this model:
-# 
-# (Hint: if you run into weird problems, maybe the state has been messed up and you can try re-runing all the code blocks from the beginning)
+#
+# (Hint: if you run into weird problems, maybe the state has been messed up and you can
+# try re-runing all the code blocks from the beginning)
 
 # In[19]:
 
 
 # now we can perform gradient descent to improve this model
 # L.Linear = chainer.links.Linear --> http://docs.chainer.org/en/stable/reference/generated/chainer.links.Linear.html
+# Linear layer (a.k.a. fully-connected layer)
 # weight matrix = W
 # bias vector = b
+# y = mx + c
+# m = W ; b = c
 model = L.Linear(in_size=1, out_size=1)
 losses = []
 
 for i in range(100):
     model.cleargrads()
+    # F.square ==> y_i = (x_i)²
+    # model: data_y = model(data_x)
     loss = F.mean(F.square(model(data_x[:, None]) - data_y[:, None]))
     losses.append(float(loss.data))
-    ## *** YOUR CODE HERE TO PERFORM GRADIENT DESCENT ***
-    ## Hint: you could access gradients with model.W.grad, model.b.grad
-    ## Hint2: you could write data into a parameter with model.W.data[:] = some_numpy_array
-    ## Hint3: if your model doesn't learn, remember to try different learning rates
+    # *** YOUR CODE HERE TO PERFORM GRADIENT DESCENT ***
+    loss.backward()
+
+    data_W_old = model.W.data[:]
+    lrate = .90
+    # .91 funny
+    if losses[-1] <= loss.data:
+        model.b.data[:] = model.b.data[:] + (1 - lrate) * loss.data
+    
+    elif losses[-1] > loss:
+        model.b.data[:] = model.b.data[:] - (1 - lrate) * loss.data
+
+    if model.W.grad <= 0:
+        model.W.data[:] = model.W.data[:] + (1 - lrate) * data_W_old
+
+    elif model.W.grad > 0:
+        model.W.data[:] = model.W.data[:] - (1 - lrate) * data_W_old
+
+
+    # Hint: you could access gradients with model.W.grad, model.b.grad
+    # Hint2: you could write data into a parameter with model.W.data[:] = some_numpy_array
+    # Hint3: if your model doesn't learn, remember to try different learning rates
     if i % 25 == 0:
         print("Itr", i, "loss:", loss)
 plt.plot(np.array(losses))
 plt.title("Learning curve")
 plt.figure()
-plt.plot(data_x, model(data_x[:, None])[:,0].data, c='r')
+plt.plot(data_x, model(data_x[:, None])[:, 0].data, c='r')
 plt.scatter(data_x, data_y, c='b')
 _ = plt.title("Trained model fitness")
 
+plt.grid()
+plt.show()
+
 
 # ## Train your first deep model
-# 
+#
 # Now we have learned the basics of Chainer. We can use it to train a deep model to classify MNIST digits. We will train a model on the MNIST dataset because the dataset is small.
-# 
+#
 # First we load the data and see what the images look like:
 
 # In[6]:
@@ -158,10 +195,10 @@ train, test = chainer.datasets.get_mnist()
 print("train[i][0] is the ith image that's flattened, and has shape:", train[12][0].shape)
 print("train[i][1] is the ith image's label, such as:", train[12][1])
 # here we visualize two of them
-plt.imshow(train[12][0].reshape([28, 28,]))
+plt.imshow(train[12][0].reshape([28, 28, ]))
 plt.title("Label: %s" % train[12][1])
 plt.figure()
-plt.imshow(train[42][0].reshape([28, 28,]))
+plt.imshow(train[42][0].reshape([28, 28, ]))
 _ = plt.title("Label: %s" % train[42][1])
 
 
@@ -171,15 +208,15 @@ _ = plt.title("Label: %s" % train[42][1])
 
 
 def run(model, batchsize=16, num_epochs=2):
-    
-    optimizer = chainer.optimizers.Adam() # we will use chainer's Adam implementation instead of writing our own gradient based optimization
+
+    optimizer = chainer.optimizers.Adam()  # we will use chainer's Adam implementation instead of writing our own gradient based optimization
     optimizer.setup(model)
-    
+
     stats = defaultdict(lambda: deque(maxlen=25))
     for epoch in range(num_epochs):
         train_iter = chainer.iterators.SerialIterator(train, batchsize, repeat=False, shuffle=True)
         test_iter = chainer.iterators.SerialIterator(test, batchsize, repeat=False, shuffle=False)
-        
+
         for itr, batch in enumerate(train_iter):
             xs = np.concatenate([datum[0][None, :] for datum in batch])
             ys = np.array([datum[1] for datum in batch])
@@ -190,7 +227,7 @@ def run(model, batchsize=16, num_epochs=2):
             model.cleargrads()
             loss.backward()
             optimizer.update()
-            
+
             # calculate stats
             stats["loss"].append(float(loss.data))
             stats["accuracy"].append(float((logits.data.argmax(1) == ys).sum() / batchsize))
@@ -219,7 +256,7 @@ class MLP(chainer.Chain):
 
     def __call__(self, x):
         # *** YOUR CODE HERE TO BUILD AN MLP W/ self.l1, self.l2, self.l3 ***
-        # 
+        #
         # Hint: you should make use of non-linearities / activation functions
         #     https://docs.chainer.org/en/stable/reference/functions.html#activation-functions
         raise NotImplementedError()
@@ -234,9 +271,9 @@ class MLP(chainer.Chain):
 
 def better_run(model, batchsize=16, num_epochs=2):
 
-    optimizer = chainer.optimizers.Adam() # we will use chainer's Adam implementation instead of writing our own gradient based optimization
+    optimizer = chainer.optimizers.Adam()  # we will use chainer's Adam implementation instead of writing our own gradient based optimization
     optimizer.setup(model)
-    
+
     stats = defaultdict(lambda: deque(maxlen=25))
     for epoch in range(num_epochs):
         train_iter = chainer.iterators.SerialIterator(train, batchsize, repeat=False, shuffle=True)
@@ -250,7 +287,7 @@ def better_run(model, batchsize=16, num_epochs=2):
             model.cleargrads()
             loss.backward()
             optimizer.update()
-            
+
             # calculate stats
             stats["loss"].append(float(loss.data))
             stats["accuracy"].append(float((logits.data.argmax(1) == ys).sum() / batchsize))
@@ -264,7 +301,7 @@ def better_run(model, batchsize=16, num_epochs=2):
 
 
 # Try different variants!
-# 
+#
 # - Does using a ConvNet improve performance (reduce overfitting?)
 # - Try changing the learning rate and observe the effect
 # - Does the model train if you give it correlated gradients? (consecutively sample many batches of "1", then many batches of "2", ... etc
